77e9929b150b0651eb477643183e740633623973
Avery-Dante <drksknddv@AveryDantesAir.fios-router.home>
2019-01-11 16:22:44 -0500

    importing generated Prisma data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
 
 // Revolvers
@@ -23,5 +24,7 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    //initializing context object that is being passed to each resolver 
+    context: { prisma },
 })
 server.start(() => console.log('Server is running  on http://localhost:4000'))
