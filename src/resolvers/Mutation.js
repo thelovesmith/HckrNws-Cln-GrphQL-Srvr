d75a7bbd4021a39b0189245108ae4e6317614e2f
Jordan Muthra <Vizionz@new-ml-011.newmuseum.org>
2019-01-29 17:37:19 -0500

    Login resovler comments

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,10 +2,10 @@
 //Signup Revolver
 async function signup(parent, args, context, info){
 
-    //encryptig users password 
+    //encrypting user's password 
     const password = await bcrypt.hash(args.password, 10)
 
-    //stor user in database using prisma client
+    //store user in database using prisma client
     const user = await context.prisma.createUser({...args, password})
 
     //generating a jwt and signing it with an APp Secret
@@ -21,18 +21,18 @@ async function signup(parent, args, context, info){
 //Login Revolver 
 async function login(parent, args, context, info){
 
-    //
+    //IF the user email isn't found in the database then the login funciton will throw an error 
     const user = await context.prisma.user({email: args.email})
     if (!user) {
         throw new Error('No such user found')
     }
 
-    //
+    //Compare the submitted password with the one in the database. If the password that was submitted doesn't match the found user then the function will throw an error
     const valid = await bcrypt.compare(args.password, user.password)
     if(!valid) {
         throw new Error('Invalid Password')
     }
-
+    //Generating another JWT token for this session with an APP_Secret. I ubdertand this as a ay to keep track of user sessions
     const token = jwt.sign({userId: user.id}, APP_SECRET)
     
     
