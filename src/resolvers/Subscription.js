d30023288428d99b08f05a07a3e702121c9eda06
Jordan Muthra <Vizionz@new-ml-011.newmuseum.org>
2019-01-30 00:48:55 -0500

    tryign to fix subscriptions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,26 +1,16 @@
-//The code seems pretty straightforward. As mentioned before, the subscription resolver is provided as the value for a subscribe field inside a plain JavaScript object.
-
-//As mentioned, the prisma client instance on the context exposes a $subscribe property which proxies the subscriptions from the Prisma API. This function is used to resolve subscriptions and push the event data. Prisma is taking care of all the complexity of handling the realtime functionality under the hood.
-
 function newLinkSubscribe(parent, args, context, info) {
-    return context.prisma.$subscribe.link({
-         mutation_in: ['CREATED'] 
-    }).node()
+    return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
 }
 
-const newLink = { 
+const newLink = {
     subscribe: newLinkSubscribe,
     resolve: payload => {
         return payload
-    }
+    },
 }
 
-
-
 function newVoteSubscribe(parent, args, context, info) {
-    return context.prisma.$subscribe.vote({ 
-        mutation_in: ['CREATED'] 
-    }).node()
+    return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
 }
 
 const newVote = {
@@ -29,7 +19,6 @@ const newVote = {
         return payload
     },
 }
-
 
 module.exports = {
     newLink,
