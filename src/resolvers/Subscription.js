379acf00cbf8263106a1dcb009b5a9239ba111f3
Avery-Dante <drksknddv@Avery-Dantes-MacBook-Air.local>
2019-01-30 00:15:35 -0500

    subscription error

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,7 @@
+//The code seems pretty straightforward. As mentioned before, the subscription resolver is provided as the value for a subscribe field inside a plain JavaScript object.
+
+//As mentioned, the prisma client instance on the context exposes a $subscribe property which proxies the subscriptions from the Prisma API. This function is used to resolve subscriptions and push the event data. Prisma is taking care of all the complexity of handling the realtime functionality under the hood.
+
 function newLinkSubscribe(parent, args, context, info) {
     return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
 }
@@ -8,10 +12,6 @@ const newLink = {
         return payload
     }
 }
-//The code seems pretty straightforward. As mentioned before, the subscription resolver is provided as the value for a subscribe field inside a plain JavaScript object.
-
-//As mentioned, the prisma client instance on the context exposes a $subscribe property which proxies the subscriptions from the Prisma API. This function is used to resolve subscriptions and push the event data. Prisma is taking care of all the complexity of handling the realtime functionality under the hood.
-
 
 
 
